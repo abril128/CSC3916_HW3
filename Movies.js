21155bab02c0aa2cf1db99cd5e47609449279926
abril ramirez <abril128lb@gmail.com>
2022-03-16 18:17:01 -0600

    commit mongo changes

--- Movies.js
+++ Movies.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-//const bcrypt = require('bcrypt-nodejs');// incript paswords
-
-mongoose.Promise = global.Promise;
-
-//mongoose.connect(process.env.DB, { useNewUrlParser: true });
-
-//mongoose.set('useCreateIndex', true);
-
-
-const MovieSchema = new Schema(
-    {
-        title: { type: String, required: true, index: { unique: true}},
-        releaseYear: { type: Number, required: true },
-        genre: { type: [String], required: true },
-        actors: { type: [String], required: true },
-    },
-    { timestamps: true },
-)
-
-module.exports = mongoose.model('movies', MovieSchema)
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+// //const bcrypt = require('bcrypt-nodejs');// incript paswords
+//
+// mongoose.Promise = global.Promise;
+//
+// //mongoose.connect(process.env.DB, { useNewUrlParser: true });
+//
+// //mongoose.set('useCreateIndex', true);
+//
+//
+// const MovieSchema = new Schema(
+//     {
+//         title: { type: String, required: true, index: { unique: true}},
+//         releaseYear: { type: Number, required: true },
+//         genre: { type: [String], required: true },
+//         actors: { type: [String], required: true },
+//     },
+//     { timestamps: true },
+// )
+//
+// module.exports = mongoose.model('movies', MovieSchema)
