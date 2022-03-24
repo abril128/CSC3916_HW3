ad2c650d1f20148bf08c6a90437c4f4624c54bc8
abril ramirez <abril128lb@gmail.com>
2022-03-24 15:50:51 -0600

    commit mongo changes

--- Movies.js
+++ Movies.js
@@ -4,7 +4,12 @@ const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
 
-//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
 
 //mongoose.set('useCreateIndex', true);
 
